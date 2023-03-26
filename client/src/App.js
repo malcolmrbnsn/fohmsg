5b7b4abfe5add6080db2f861c66bea683ab510a8
Malcolm Robinson <malcolmrbnsn@outlook.com>
2023-03-26 16:39:56 +1100

    added basic display container

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import socketIO from 'socket.io-client';
 import ChatPage from './components/ChatPage';
+import './App.css';
 
 const socket = socketIO.connect("http://localhost:3001");
 
@@ -9,7 +10,7 @@ const socket = socketIO.connect("http://localhost:3001");
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="container">
         <Routes>
           <Route path="/" element={ <Home socket={socket}/> } />
           <Route path="/chat" element={ <ChatPage socket={socket}/> } />
