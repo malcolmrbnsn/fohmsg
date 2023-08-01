fc02eee00b4232bcfda120e823ed5288c496ec6b
Malcolm Robinson <malcolmrbnsn@outlook.com>
2023-08-01 12:46:08 +1000

    server works ??

--- server/chatroom.js
+++ server/chatroom.js
@@ -1,71 +1,71 @@
 class Chatroom {
-    constructor(chatroomID) {
-      this.chatroomID = chatroomID;
-      this.users = [];
-      this.possibleStatuses = ["Offline", "Online", "Error"]
-    }
-  
-    addUser(userID, username) {
-      const user = {
-        userID,
-        username,
-        status: 1, // 0: offline, 1: online, 2: error
-        messages: []
-      };
-      this.users.push(user);
-    }
-  
-    getUser(userID) {
-      return this.users.find(user => user.userID === userID);
-    }
-  
-    addMessage(userID, text) {
-      const user = this.getUser(userID);
-      if (user) {
-        const message = {
-          messageID: `message_${Date.now()}`,
-          text,
-          timestamp: new Date().toISOString()
-        };
-        user.messages.push(message);
-      }
-    }
-
-    setUserStatus(userID, newStatus) {
-        const user = this.getUser(userID);
-        if (user) {
-          user.status = newStatus;
-        }
-      }
-  
-    getMessages(userID) {
-      const user = this.getUser(userID);
-      return user ? user.messages : [];
-    }
-
-    getLastMessages() {
-      const usersWithLastMessage = this.users.map(user => {
-        const lastMessageIndex = user.messages.length - 1;
-        const lastMessage = lastMessageIndex >= 0 ? user.messages[lastMessageIndex] : null;
-  
-        return {
-          userID: user.userID,
-          username: user.username,
-          lastMessage,
-          status: this.possibleStatuses[user.status]
-        };
-      });
-  
-      return usersWithLastMessage;
-    }
-
-    debug() {
-      return this.users;
-    }
-  
-    getChatroomID() {
-      return this.chatroomID;
-    }
+  constructor(chatroomID) {
+    this.chatroomID = chatroomID;
+    this.users = [];
+    this.possibleStatuses = ["Offline", "Online", "Error"]
   }
+
+  addUser(userID, username) {
+    const user = {
+      userID,
+      username,
+      status: 1, // 0: offline, 1: online, 2: error
+      messages: []
+    };
+    this.users.push(user);
+  }
+
+  getUserByID(userID) {
+    return this.users.find(user => user.userID === userID);
+  }
+
+  getUserByUsername(username) {
+    return this.users.find(user => user.username === username);
+  }
+
+  updateUser(newUser) {
+      let toReplace = this.getUserByID(newUser.userID);
+      Object.assign(toReplace, newUser);
+  }
+
+  addMessage(userID, text) {
+    const user = this.getUser(userID);
+    if (user) {
+      const message = {
+        messageID: `message_${Date.now()}`,
+        text,
+        timestamp: new Date().toISOString()
+      };
+      user.messages.push(message);
+    }
+    this.updateUser(user);
+  }
+
+  setUserStatus(userID, newStatus) {
+    const user = this.getUserByID(userID);
+    if (user) {
+      user.status = newStatus;
+    }
+
+    this.updateUser(user);
+  }
+
+  getMessages(userID) {
+    const user = this.getUser(userID);
+    return user ? user.messages : [];
+  }
+
+  getData() {
+    return this.users;
+  }
+
+  debug() {
+    return this.users;
+  }
+
+  getChatroomID() {
+    return this.chatroomID;
+  }
+}
 
 module.exports = Chatroom
--- server/index.js
+++ server/index.js
@@ -27,33 +27,43 @@ socketIO.on('connection', (socket) => {
 
     socket.on('joinChatroom', data => {
       const {username} = data;
-      if (chatroom.getUser(socket.id)) {
-        chatroom.setUserStatus(socket.id, 1);
+      foundUser = chatroom.getUserByID(socket.id);
+      if (foundUser) {
+        foundUser.username = username;
+        foundUser.status = 1;
+        chatroom.updateUser(foundUser);
       } else {
         chatroom.addUser(socket.id, username);
       }
+      // if (chatroom.getUserByID(socket.id) || chatroom.getUserByUsername(username)){
+        // chatroom.setUserStatus(socket.id, 1);
+      // } else {
+        // chatroom.addUser(socket.id, username);
+      // }
       
       console.log(`CONN: ${socket.id} logged in as ${username}`);
-      socketIO.emit('push', chatroom.getLastMessages());
+      socketIO.emit('push', chatroom.getData());
       // TODO: if username exists, replace socketID and set online>??? 
     });
 
     socket.on('sendMessage', data => {
       chatroom.addMessage(socket.id, data.message);
       console.log(`CONN: message ${data.text} from ${data.username}`);
-      socketIO.emit('push', chatroom.getLastMessages());
+      socketIO.emit('push', chatroom.getData());
     });
 
     socket.on('leaveChatroom', data => {
       chatroom.setUserStatus(socket.id, 0);
-      socketIO.emit('push', chatroom.getLastMessages());
+      socketIO.emit('push', chatroom.getData());
 
     })
 
     socket.on('disconnect', () => {
       console.log(`CONN: ${socket.id} disconnected`);
-      chatroom.setUserStatus(socket.id, 0);
-      socketIO.emit('push', chatroom.getLastMessages());
+      if (chatroom.getUserByID(socket.id)){
+        chatroom.setUserStatus(socket.id, 0);
+    }
+      socketIO.emit('push', chatroom.getData());
     });
 });
 
